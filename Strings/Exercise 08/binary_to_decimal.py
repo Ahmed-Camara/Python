0398bd622afb3e404bb0080317de5b5596ec7ddb
Ahmed-Camara <camarahmed123@gmail.com>
2021-09-03 14:29:29 +0600

    Exercise 08 (binary to decimal)

--- Strings/Exercise 08/binary_to_decimal.py
+++ Strings/Exercise 08/binary_to_decimal.py
@@ -1,3 +1,13 @@
+"""
+(Binary to decimal) Write a function that parses a binary number as a string into a
+decimal integer. Use the function header:
+def binaryToDecimal(binaryString):
+For example, binary string 10001 is 17 
+So, binaryToDecimal("10001") returns 17.
+Write a test program that prompts the user to enter a binary string and displays the
+corresponding decimal integer value.
+"""
+
 import math
 
 

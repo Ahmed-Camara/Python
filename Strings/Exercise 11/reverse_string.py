7fb0a930c2fce99c4fb25d73afd9082e320312ab
Ahmed-Camara <camarahmed123@gmail.com>
2021-09-03 16:14:00 +0600

    Exercise 11 (reverse string)

--- Strings/Exercise 11/reverse_string.py
+++ Strings/Exercise 11/reverse_string.py
@@ -1,3 +1,11 @@
+"""
+(Reverse a string) Write a function that reverses a string. The header of the function is:
+def reverse(s):
+Write a test program that prompts the user to enter a string, invokes the reverse
+function, and displays the reversed string
+"""
+
+
 def reverse(str):
 
     c = -1
